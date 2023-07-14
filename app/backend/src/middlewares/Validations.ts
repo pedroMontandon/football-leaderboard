d1f587eff132ba5eb74841f3235535e5ff227026
pedroMontandon <pedrobarbosa89@gmail.com>
2023-07-13 21:49:11 -0300

    password validation implemented

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -2,10 +2,16 @@ import { NextFunction, Request, Response } from 'express';
 import JwtUtils from '../utils/JwtUtils';
 
 export default class Validations {
+  private static passwordLength = 6;
+  private static emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
   static validateLogin(req: Request, res: Response, next: NextFunction) {
-    const { email, password } = req.body;
+    const { email, password } = req.body as { email: string; password: string };
     if (!email || !password) {
       return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    if (password.length < Validations.passwordLength || !Validations.emailRegex.test(email)) {
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
     return next();
   }
